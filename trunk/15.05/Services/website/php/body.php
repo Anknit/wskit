4bfc29a1e67dea73f5dd3e217d7f8041e4598fa0
Anknit <Anknit@3fae0dfe-ad2e-44ac-bc64-e54151dd7069>
2015-06-10 05:48:01 +0000

    signup page added

--- trunk/15.05/Services/website/php/body.php
+++ trunk/15.05/Services/website/php/body.php
@@ -5,9 +5,14 @@
 	include './html/searchContainer.html';
 ?>
 <div class='content-webservice container-fluid col-md-12'>
-<?php 
-	include './html/leftPanel.html';
-	include './html/rightPanel.html';
+<?php
+	if(isset($_GET['signup'])){
+		include './html/signup.html';
+	} 
+	else{
+		include './html/leftPanel.html';
+		include './html/rightPanel.html';
+	}
 ?>
 </div>
 </div>
